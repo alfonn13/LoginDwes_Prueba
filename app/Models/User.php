5479e8222e8d596d76103ed711e87e484f73cc0d
unknown <alfonsoreviejovalle@alumnos.tuniversformacion.es>
2024-01-30 12:32:24 +0100

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function tuits(){
+        return $this->hasMany(Tuit::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
